af12e46ff025dfd78a3e61e3c3779dfc19859c76
Marek Kawczyński <mkawczynski07@gmail.com>
2015-09-21 11:01:50 +0200

    add total to event

--- src/chart.js
+++ src/chart.js
@@ -1,7 +1,7 @@
 (function () {
-    
+
     var SVG_NAMESPACE = "http://www.w3.org/2000/svg";
-    
+
     var PieChart = function (opts) {
         var me = this, $chart,
                 size = opts.size || opts.$container.offsetWidth,
@@ -15,7 +15,7 @@
                 sin = Math.sin,
                 animated = !isDefined(opts.animated) ? true : opts.animated,
                 drawAtStart = !isDefined(opts.drawAtStart) ? true : opts.drawAtStart;
-        
+
         createChart();
         createBacgroundRing();
         addPaths();
@@ -25,7 +25,7 @@
         if (drawAtStart) {
             drawPaths();
         }
-        
+
         me.update = function (data) {
             loopOverSectors(function (sector) {
                 setNewSectorValue(sector, data[sector.name]);
@@ -33,11 +33,12 @@
             });
             drawPaths();
         };
-        
+
         function drawPaths() {
-            executeEvent('onUpdateStart');
             calculateTotal();
             calculatePercents();
+            executeEvent('onUpdateStart');
+
             if (animated === true) {
                 drawWithAnimation();
             } else {
@@ -45,7 +46,7 @@
                 executeEvent('onUpdateEnd');
             }
         }
-        
+
         function drawWithAnimation() {
             var startAngle = -PI / 2, endAngle = 0,
                     sectors = opts.definition,
@@ -53,7 +54,7 @@
                     counter = 0;
             anim(startAngle, length, counter);
         }
-        
+
         function anim(startAngle, length, counter) {
             var sector = opts.definition[counter], timeout, currentValue = 0, newStartAngle = 0,
                     timeoutFn = function () {
@@ -74,7 +75,7 @@
                     };
             timeout = setTimeout(timeoutFn, durration);
         }
-        
+
         function draw() {
             var startAngle = -PI / 2;
             loopOverSectors(function (sector) {
@@ -83,7 +84,7 @@
                 }
             });
         }
-        
+
         function updatePath($path, startAngle, value) {
             var sectorAngle = (value / 100) * (PI * 2),
                     endRadius = startAngle + sectorAngle,
@@ -101,7 +102,7 @@
             $path.setAttribute("d", d.join(' '));
             return sectorAngle;
         }
-        
+
         function calculateTotal() {
             total = 0;
             loopOverSectors(function (sector) {
@@ -109,7 +110,7 @@
                 total += sector.value;
             });
         }
-        
+
         function  calculatePercents() {
             totalPercent = 0;
             if (total === 0) {
@@ -123,18 +124,18 @@
                 }
             });
         }
-        
+
         function addPaths() {
             loopOverSectors(addPath);
         }
-        
+
         function addPath(sector) {
             var $path = createSvgElement('path');
             $path.setAttributeNS(null, "class", sector.cls);
             $chart.appendChild($path);
             sector.$path = $path;
         }
-        
+
         function loopOverSectors(fn) {
             var x = 0, definition = opts.definition,
                     length = definition.length;
@@ -142,26 +143,26 @@
                 fn.call(me, definition[x], x);
             }
         }
-        
+
         function createChart() {
             $chart = createSvgElement("svg:svg");
             $chart.setAttribute("width", size);
             $chart.setAttribute("height", size);
             $chart.setAttribute("viewBox", "0 0 " + size + " " + size);
         }
-        
+
         function createBacgroundRing() {
             var $bacground = createCircle(centerOfTheCircle, centerOfTheCircle);
             $bacground.setAttributeNS(null, "class", "ring-chart-background");
             $chart.appendChild($bacground);
         }
-        
+
         function createEmptyBackgroundRing() {
             var $emptyCircle = createCircle(centerOfTheCircle, size * ringProportion);
             $emptyCircle.setAttributeNS(null, "class", "ring-chart-background-empty");
             $chart.appendChild($emptyCircle);
         }
-        
+
         function createCircle(size, radius) {
             var circle = createSvgElement("circle");
             circle.setAttributeNS(null, "cx", size);
@@ -169,33 +170,33 @@
             circle.setAttributeNS(null, "r", radius);
             return circle;
         }
-        
+
         function createSvgElement(type) {
             return document.createElementNS(SVG_NAMESPACE, type);
         }
-        
+
         function shouldDrawSection(section) {
             return section.value > 0 && section.percent > 0;
         }
-        
+
         function isDefined(val) {
             return typeof val !== 'undefined';
         }
-        
+
         function setNewSectorValue(sector, value) {
             if (isDefined(value)) {
                 sector.value = parseInt(value);
             }
         }
-        
+
         function  removePathPie($path) {
             $path.removeAttribute('d');
         }
-        
+
         function isLastSector(index) {
             return index + 1 === opts.definition.length;
         }
-        
+
         function getSectorNextAnimValue(sector, currentValue) {
             var percent = sector.percent, step = speed;
             if (currentValue + speed > percent) {
@@ -206,20 +207,21 @@
             }
             return step;
         }
-        
+
         function executeEvent(name) {
             var fn = opts[name];
             if (typeof fn === 'function') {
                 fn.call(me, {
                     $chart: $chart,
-                    chart: me
+                    chart: me,
+                    total: total
                 });
             }
         }
-        
+
     };
-    
-    
+
+
     var $container = document.getElementById('chart'),
             chart = new PieChart({
                 $container: $container,
@@ -239,5 +241,5 @@
                     console.log(arguments);
                 }
             });
-    
+
 })();
